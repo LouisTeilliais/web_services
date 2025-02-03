4a970a21aadc57109a6026a6c2f15d4f423f0737
Louis Teilliais <louis.teilliais@gmail.com>
2025-02-03 09:57:22 +0100

    add prisma & swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Web services')
+    .setDescription('The web services API description')
+    .setVersion('1.0')
+    .addTag('web_services')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
