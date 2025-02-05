f35751b33d1f47c81f717c8c4c6e38217cd1f3fc
taobourmaud <tao.bourmaud@ynov.com>
2025-02-05 15:52:21 +0100

    test unitaire avant de déployer

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Bonjour, Tao a réussi à publier!';
+    return 'Hello World!';
   }
 }
